32188a933cb3e0c4496e1cc6f31e88af62a369f5
Franck <franck.yeboa@gmail.com>
2020-06-02 23:01:54 +0100

    Updates

---
+++ precache-manifest.21d5daa947ddb9afd2cb2c16d71e9a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18e3377e486765d6a1efe32c9ecf19b",
+    "url": "/appcontacts/index.html"
+  },
+  {
+    "revision": "f34aeeb61a112b14b338",
+    "url": "/appcontacts/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f34aeeb61a112b14b338",
+    "url": "/appcontacts/static/js/2.0e1f976a.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/appcontacts/static/js/2.0e1f976a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d028d427ed50b0ff68d5",
+    "url": "/appcontacts/static/js/main.7ed67966.chunk.js"
+  },
+  {
+    "revision": "21350c0b39e198c9ed24",
+    "url": "/appcontacts/static/js/runtime-main.aaa37d23.js"
+  }
+]);
